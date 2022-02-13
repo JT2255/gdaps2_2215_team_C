1fe1a441d28dcb11e9d29a04c1c26dd1e837b44e
Joe <jkt1886@rit.edu>
2022-02-13 14:10:48 -0500

    added base classes

---
+++ game/MurderMystery/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MurderMystery
+{
+    class Inventory
+    {
+        // ~~~ FIELDS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        #region Fields
+        #endregion
+        // ~~~ PROPERTIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        #region Properties
+        #endregion
+        // ~~~ CONSTRUCTORS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        #region Constructors
+        #endregion
+        // ~~~ METHODS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        #region Methods
+        #endregion
+        // ~~~ OVERRIDES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        #region Overrides
+        #endregion
+    }
+}
